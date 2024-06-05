77c0539e719be9bff04f1c88029ef76d5c2f953d
dianamutinda <jaydiana25@gmail.com>
2024-06-05 16:47:52 +0300

    creates the about section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
  import Hero from "./Hero";
+ import About from "./About";
  import "./Home.css"
 const Home =() =>{
     return(
         <>
         <Hero/>
+        <About/>
         </>
     )
 }
