89e0439fbf1aa35b9a1a9e0fae6255a1f1389004
dianamutinda <jaydiana25@gmail.com>
2024-06-05 12:34:57 +0300

    creates the header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import Headertop from "./Headertop";
+import Headerbottom from "./Headerbottom";
+import "./Header.css"
+const Header = () =>{
+    return(
+        <>
+        <Headertop />
+        <Headerbottom/>
+
+        </>
+    )
+}
+
+export default Header;
---
+++ src/Components/Headertop.jsx
@@ -0,0 +1,11 @@
+
+const Headertop = () =>{
+    return(
+        <div className="headertop">
+            <h1 className="logo">Zaph Tours</h1>
+            <h4 className="tel-text">call us on toll free: <span className="tel-num"><a href="" className="tel-num">+254700123654</a></span></h4>
+        </div>
+    )
+}
+
+export default Headertop;
