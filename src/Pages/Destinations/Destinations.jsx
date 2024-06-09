84d7a7218f0c37b644016f54d5e36611887dc2d2
dianamutinda <jaydiana25@gmail.com>
2024-06-09 12:21:42 +0300

    creates destination images

--- src/Pages/Destinations/Destinations.jsx
+++ src/Pages/Destinations/Destinations.jsx
@@ -1,3 +1,11 @@
+import dubaiImg from "../../assets/dubai.jpeg"
+import hellImg from "../../assets/Hells Gate.jpeg"
+import coloradoImg from "../../assets/colorado springs.jpeg"
+import karuraImg from "../../assets/karura.jpeg"
+import menengaiImg from "../../assets/Menengai crater.jpeg"
+import mombasaImg from "../../assets/mombasa.jpeg"
+import parisImg from "../../assets/paris.jpeg"
+import sevenImg from "../../assets/seven falls.jpeg"
 import bgImg from "../../assets/bg.jpeg"
 import "./Destinations.css"
 const Destinations = () =>{
@@ -5,8 +13,24 @@ const Destinations = () =>{
         <section>
             <div className="destination">
             <div className="dest-container">
+                <div className="img-wrapper">
+                    <img src={dubaiImg} alt=""  />
                 <div className="dest-text">
-                    <h2 className="place-name">diani</h2>
+                    <h2 className="place-name">dubai</h2>
+                    <p>Lorem ipsum,Debitis vero aperiam, magni possimus ut voluptatibus esse unde modi eveniet aliquid.</p>
+                    <a href="" className="cta">view more</a>
+                    <p className="price">price @ person $200 </p>
+                    <p className="price">price group $190</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="dest-container">
+                <div className="img-wrapper">
+                    <img src={coloradoImg} alt=""  />
+                </div>
+                <div className="dest-text">
+                    <h2 className="place-name">colorado springs</h2>
                     <p>Lorem ipsum,Debitis vero aperiam, magni possimus ut voluptatibus esse unde modi eveniet aliquid.</p>
                     <a href="" className="cta">view more</a>
                     <p className="price">price @ person $200 </p>
@@ -15,8 +39,11 @@ const Destinations = () =>{
             </div>
 
             <div className="dest-container">
+                <div className="img-wrapper">
+                    <img src={hellImg} alt=""  />
+                </div>
                 <div className="dest-text">
-                    <h2 className="place-name">diani</h2>
+                    <h2 className="place-name">hells gate</h2>
                     <p>Lorem ipsum,Debitis vero aperiam, magni possimus ut voluptatibus esse unde modi eveniet aliquid.</p>
                     <a href="" className="cta">view more</a>
                     <p className="price">price @ person $200 </p>
@@ -25,8 +52,11 @@ const Destinations = () =>{
             </div>
 
             <div className="dest-container">
+                <div className="img-wrapper">
+                    <img src={menengaiImg} alt=""  />
+                </div>
                 <div className="dest-text">
-                    <h2 className="place-name">diani</h2>
+                    <h2 className="place-name">Menengai crater</h2>
                     <p>Lorem ipsum,Debitis vero aperiam, magni possimus ut voluptatibus esse unde modi eveniet aliquid.</p>
                     <a href="" className="cta">view more</a>
                     <p className="price">price @ person $200 </p>
@@ -35,8 +65,24 @@ const Destinations = () =>{
             </div>
 
             <div className="dest-container">
+                <div className="img-wrapper">
+                    <img src={mombasaImg} alt=""  />
                 <div className="dest-text">
-                    <h2 className="place-name">diani</h2>
+                    <h2 className="place-name">mombasa</h2>
+                    <p>Lorem ipsum,Debitis vero aperiam, magni possimus ut voluptatibus esse unde modi eveniet aliquid.</p>
+                    <a href="" className="cta">view more</a>
+                    <p className="price">price @ person $200 </p>
+                    <p className="price">price group $190</p>
+                  </div>
+                </div>
+            </div>
+
+            <div className="dest-container">
+                <div className="img-wrapper">
+                    <img src={karuraImg} alt=""  />
+                </div>
+                <div className="dest-text">
+                    <h2 className="place-name">karura forest</h2>
                     <p>Lorem ipsum,Debitis vero aperiam, magni possimus ut voluptatibus esse unde modi eveniet aliquid.</p>
                     <a href="" className="cta">view more</a>
                     <p className="price">price @ person $200 </p>
@@ -45,8 +91,11 @@ const Destinations = () =>{
             </div>
 
             <div className="dest-container">
+                <div className="img-wrapper">
+                    <img src={parisImg} alt=""  />
+                </div>
                 <div className="dest-text">
-                    <h2 className="place-name">diani</h2>
+                    <h2 className="place-name">paris</h2>
                     <p>Lorem ipsum,Debitis vero aperiam, magni possimus ut voluptatibus esse unde modi eveniet aliquid.</p>
                     <a href="" className="cta">view more</a>
                     <p className="price">price @ person $200 </p>
@@ -55,8 +104,11 @@ const Destinations = () =>{
             </div>
 
             <div className="dest-container">
+                <div className="img-wrapper">
+                    <img src={sevenImg} alt=""  />
+                </div>
                 <div className="dest-text">
-                    <h2 className="place-name">diani</h2>
+                    <h2 className="place-name">seven falls</h2>
                     <p>Lorem ipsum,Debitis vero aperiam, magni possimus ut voluptatibus esse unde modi eveniet aliquid.</p>
                     <a href="" className="cta">view more</a>
                     <p className="price">price @ person $200 </p>
@@ -65,26 +117,9 @@ const Destinations = () =>{
             </div>
 
             <div className="dest-container">
-                <div className="dest-text">
-                    <h2 className="place-name">diani</h2>
-                    <p>Lorem ipsum,Debitis vero aperiam, magni possimus ut voluptatibus esse unde modi eveniet aliquid.</p>
-                    <a href="" className="cta">view more</a>
-                    <p className="price">price @ person $200 </p>
-                    <p className="price">price group $190</p>
+                <div className="img-wrapper">
+                    <img src={bgImg} alt=""  />
                 </div>
-            </div>
-
-            <div className="dest-container">
-                <div className="dest-text">
-                    <h2 className="place-name">diani</h2>
-                    <p>Lorem ipsum,Debitis vero aperiam, magni possimus ut voluptatibus esse unde modi eveniet aliquid.</p>
-                    <a href="" className="cta">view more</a>
-                    <p className="price">price @ person $200 </p>
-                    <p className="price">price group $190</p>
-                </div>
-            </div>
-
-            <div className="dest-container">
                 <div className="dest-text">
                     <h2 className="place-name">diani</h2>
                     <p>Lorem ipsum,Debitis vero aperiam, magni possimus ut voluptatibus esse unde modi eveniet aliquid.</p>
